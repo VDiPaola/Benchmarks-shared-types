51ff9db446f928421c18803fd4c7cd90066b38d7
Enzo <dipaolae98@gmail.com>
2025-01-21 19:36:19 +0000

    db update

--- src/types/DatabaseTypes/index.ts
+++ src/types/DatabaseTypes/index.ts
@@ -3,3 +3,21 @@ export type DatabaseBenchmarkType = {
     name: string;
     data: string;
 }
+
+export type userScoreDBType = {
+    id: string;
+    userId: string;
+    score: number;
+}
+
+export type userDBType = {
+    id: string;
+    role: string;
+    scores: string;
+}
+
+export type adminDBType = {
+    id: string;
+    discordId: string;
+    name: string;
+}
