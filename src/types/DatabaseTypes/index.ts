fe58cb337eb29d6ffde5d9248df2ac9a1a998572
Enzo <dipaolae98@gmail.com>
2025-01-22 14:03:07 +0000

    database and kovaaks types

--- src/types/DatabaseTypes/index.ts
+++ src/types/DatabaseTypes/index.ts
@@ -2,22 +2,30 @@ export type DatabaseBenchmarkType = {
     id: string;
     name: string;
     data: string;
+    created_at: number;
+    updated_at: number;
 }
 
 export type DatabaseUserBenchmarkScoreType = {
     id: string;
-    userId: string;
+    user_id: string;
     score: number;
+    created_at: number;
+    updated_at: number;
 }
 
 export type DatabaseUserType = {
     id: string;
-    role: string;
-    scores: string;
+    user_id: string;
+    discord_id: string;
+    kovaaks_username: string;
+    role: "user" | "admin";
+    created_at: number;
 }
 
 export type DatabaseAdminType = {
     id: string;
-    discordId: string;
+    discord_id: string;
     name: string;
+    created_at: number;
 }
--- src/types/KovaaksTypes/index.ts
+++ src/types/KovaaksTypes/index.ts
@@ -82,3 +82,59 @@ export type KovaaksLeaderboardAttributesType = {
     sensScale: string;
     accuracyDamage: number;
 }
+
+export type KovaaksProfileDataType = {
+    playerId: number;
+    steamAccountName: string;
+    steamAccountAvatar: string;
+    created: string; // ISO Date string
+    steamId: string;
+    clientBuildVersion: string;
+    lastAccess: string; // ISO Date string
+    webapp: {
+      roles: {
+        admin: boolean;
+        coach: boolean;
+        staff: boolean;
+      };
+      videos: any[]; // Array of videos (currently empty, specify type if known)
+      username: string;
+      socialMedia: {
+        tiktok: string | null;
+        twitch: string | null;
+        discord: string | null;
+        twitter: string | null;
+        youtube: string | null;
+        discord_id: string;
+      };
+      gameSettings: {
+        dpi: number | null;
+        fov: string;
+        cm360: string;
+        rawInput: string;
+        sensitivity: number | null;
+      };
+      profileImage: string | null;
+      profileViews: number;
+      hasSubscribed: boolean;
+      gamingPeripherals: {
+        mouse: string;
+        headset: string | null;
+        monitor: string | null;
+        keyboard: string | null;
+        mousePad: string | null;
+      };
+    };
+    country: string;
+    kovaaksPlusActive: boolean;
+    badges: any[]; // Array of badges (currently empty, specify type if known)
+    followCounts: {
+      following: number;
+      followers: number;
+    };
+    kovaaksPlus: {
+      active: boolean;
+      expiration: string; // ISO Date string
+    };
+    scenariosPlayed: string;
+}
