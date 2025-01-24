fdf7fd49635b2499acd1d6f1f236bdc93ef60aea
Enzo <dipaolae98@gmail.com>
2025-01-24 18:40:31 +0000

    optional properties

--- src/types/DatabaseTypes/index.ts
+++ src/types/DatabaseTypes/index.ts
@@ -2,30 +2,30 @@ export type DatabaseBenchmarkType = {
     id: string;
     name: string;
     data: string;
-    created_at: number;
-    updated_at: number;
+    created_at?: number;
+    updated_at?: number;
 }
 
 export type DatabaseUserBenchmarkScoreType = {
-    id: string;
+    id?: string;
     user_id: string;
     score: number;
-    created_at: number;
-    updated_at: number;
+    created_at?: number;
+    updated_at?: number;
 }
 
 export type DatabaseUserType = {
-    id: string;
+    id?: string;
     user_id: string;
     discord_id: string;
     kovaaks_username: string;
     role: "user" | "admin";
-    created_at: number;
+    created_at?: number;
 }
 
 export type DatabaseAdminType = {
-    id: string;
+    id?: string;
     discord_id: string;
     name: string;
-    created_at: number;
+    created_at?: number;
 }
